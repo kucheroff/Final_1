db7945d46eac0facbf765f5c70b2b8ff54d760d1
vyacheslav <kucheroff@tut.by>
2023-04-03 17:01:08 +0300

    добавлен метод красивого вывода массива (скобки и кавычки)

--- Program.cs
+++ Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿void printArray(string[] array)
+{
+    Console.Write("[");
+
+    for (int i = 0; i<array.Length; i++)
+    {
+        if (i<array.Length-1) Console.Write($"\"{array[i]}\", ");
+        else Console.Write($"\"{array[i]}\"");
+    }
+    Console.WriteLine("]");
+}
+
+
+
+Console.Clear();
+Console.WriteLine("Исходный массив:");
+string[] array= new string[4] {"hello","2","world",":-)"};
+printArray(array);
