be2bf8873a92c64be8e9281e12eee26e2c369b5d
vyacheslav <kucheroff@tut.by>
2023-04-03 17:04:32 +0300

    добавлен метод заполнения нового массива элементами с нужным кол-вом символов

--- Program.cs
+++ Program.cs
@@ -1,10 +1,33 @@
-﻿void printArray(string[] array)
+﻿string[] fillToThree(string[] array)
+{
+    int count = 0;
+    for (int i = 0; i<array.Length; i++) // Обнаруживаем размерность итогового массива
+    {
+        if(array[i].Length<=3) count++;
+    }
+
+    string[] new_array = new string[count]; // Делаем итоговй массив нужной длины
+    count = 0;
+
+    for (int i = 0; i < array.Length; i++)  // Заполняем итоговый массив нужными элементами
+    {
+        if(array[i].Length <=3 ) 
+        {
+            new_array[count] = array[i];
+            count++;
+        }
+    }
+
+    return new_array;
+}
+
+void printArray(string[] array)
 {
     Console.Write("[");
 
     for (int i = 0; i<array.Length; i++)
     {
-        if (i<array.Length-1) Console.Write($"\"{array[i]}\", ");
+        if (i<array.Length-1) Console.Write($"\"{array[i]}\", "); // красивый вывод со скобками и кавычками
         else Console.Write($"\"{array[i]}\"");
     }
     Console.WriteLine("]");
